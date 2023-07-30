d6e577a02d4c927e179885453ad71fbd9f7d690d
rahulkgautam <rahulkgautam125@gmail.com>
2023-07-30 22:35:34 +0530

    Find Frequency of word in a sentence

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace HashTableProblem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hash table program...");
+            string paragraph = "To be or not to be";
+            CountNumbOfOccurence(paragraph);
+        }
+        public static void CountNumbOfOccurence(string paragraph)
+        {
+            MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+
+            string[] words = paragraph.Split(' ');
+
+            foreach (string word in words)
+            {
+                if (hashTabe.Exists(word.ToLower()))
+                    hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                else
+                    hashTabe.Add(word.ToLower(), 1); 
+            }
+            Console.WriteLine("Displaying after add operation");
+            hashTabe.Display();
         }
     }
 }
